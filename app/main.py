6a8bf6042ed39fadae9271dc443dc243b0cf05db
rffffy <raafay.alam@gmail.com>
2022-06-14 01:10:28 +0200

    added jwt token functionality

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from opcode import hasconst
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from . import models
-from .database import engine, get_db
+from .database import engine
 from .routers import post, user, auth
 
 
