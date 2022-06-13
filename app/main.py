5a0819664a23def6db943f7ad5c39fd43192c204
rffffy <raafay.alam@gmail.com>
2022-06-13 12:31:23 +0200

    added comments

--- app/main.py
+++ app/main.py
@@ -38,15 +38,23 @@ def root():
     return {"message": "Welcome to my api!"}
 
 
+# Get all Posts
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
+    """
+    Get method, to get all the posts
+    """
     posts = db.query(models.Post).all()
     
     return {"data": posts}
 
 
+# Create a Post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    """
+    Post method, to create a post
+    """
     new_post = models.Post(**post.dict())
 
     db.add(new_post)
@@ -56,8 +64,12 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return {"new_post": new_post}
 
 
+# Get a Post filtered by id
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+    """
+    Get method, to get a Post filtered by id
+    """
     post = db.query(models.Post).filter(models.Post.id == id).first()
     
     if not post:
@@ -67,8 +79,12 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
     return {"post_detail": post}
 
 
+# Delete a Post filtered by id
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
+    """
+    Delete method, to delete a Post filtered by id
+    """
     post_query = post = db.query(models.Post).filter(models.Post.id == id)
 
     if post_query.first() == None:
@@ -81,8 +97,12 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+# Update a Post filtered by id
 @app.put("/posts/{id}")
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    """
+    Put method, to update a Post filtered by id
+    """
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post_to_update = post_query.first()
 
