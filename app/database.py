2652c62b4f805cdd3421abcdddb591958601468b
rffffy <raafay.alam@gmail.com>
2022-06-14 15:41:47 +0200

    added environment variables

--- app/database.py
+++ app/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
 # import psycopg2
 # from psycopg2.extras import RealDictCursor
 # import time
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://raafayalam:root@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@\
+{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
